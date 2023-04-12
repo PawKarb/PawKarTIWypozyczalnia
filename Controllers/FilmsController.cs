eaff6daef580035dd8d6b2d4e299bf8eb3c9c189
Paweł Karbowski <pauloka09@gmail.com>
2023-04-12 14:51:25 +0200

    Dodawanie filmów wraz z plakatem

--- Controllers/FilmsController.cs
+++ Controllers/FilmsController.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using PawKarTIWypozyczalnia.DAL;
 using PawKarTIWypozyczalnia.Models;
 using PawKarTIWypozyczalnia.ViewModels;
+using System.IO;
 using System.Linq;
 
 namespace PawKarTIWypozyczalnia.Controllers
@@ -10,10 +12,12 @@ namespace PawKarTIWypozyczalnia.Controllers
     public class FilmsController : Controller
     {
         FilmsContext db;
+        IWebHostEnvironment env;
 
-        public FilmsController(FilmsContext db)
+        public FilmsController(FilmsContext db, IWebHostEnvironment env)
         {
             this.db = db;
+            this.env = env;
         }
 
         public IActionResult FilmsList(string categoryName)
@@ -44,6 +48,28 @@ namespace PawKarTIWypozyczalnia.Controllers
         {
             var films = db.Films.OrderBy(f => f.Date);
             return View(films);
+        }
+        [HttpGet]
+        public IActionResult AddFilm()
+        {
+            var model = new AddFilmViewModel();
+            var categories = db.Kategorie.ToList();    
+
+            model.category = categories;
+
+            return View(model); 
+        }
+        [HttpPost]
+        public IActionResult AddFilm(AddFilmViewModel model)
+        {
+            var folderPath = Path.Combine(env.WebRootPath, "content");
+            var posterPath = Path.Combine(folderPath, model.Poster.FileName);
+            model.Poster.CopyTo(new FileStream(posterPath,FileMode.Create));
+            model.newFilm.Date = System.DateTime.Now;
+            model.newFilm.PosterName = model.Poster.FileName;
+            db.Films.Add(model.newFilm);
+            db.SaveChanges();
+            return RedirectToAction("Indeks", "Home");
         }
     }
 }
---
+++ ViewModels/AddFilmViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using PawKarTIWypozyczalnia.Models;
+using System.Collections.Generic;
+
+namespace PawKarTIWypozyczalnia.ViewModels
+{
+    public class AddFilmViewModel
+    {
+        public Film newFilm { get; set; }
+        public List<Kategoria> category { get; set; }
+        public IFormFile Poster { get; set; }
+    }
+}
