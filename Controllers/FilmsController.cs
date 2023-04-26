cf955d2609da43f7f5b3daf7a43e3e6c3d152382
Paweł Karbowski <pauloka09@gmail.com>
2023-04-26 13:21:22 +0200

    dodanie edycji i wyszukiwanie filmów

--- Controllers/FilmsController.cs
+++ Controllers/FilmsController.cs
@@ -4,6 +4,7 @@ using Microsoft.EntityFrameworkCore;
 using PawKarTIWypozyczalnia.DAL;
 using PawKarTIWypozyczalnia.Models;
 using PawKarTIWypozyczalnia.ViewModels;
+using System;
 using System.IO;
 using System.Linq;
 
@@ -33,18 +34,18 @@ namespace PawKarTIWypozyczalnia.Controllers
             model.RecentFilms = db.Films.OrderByDescending(f => f.Date).Take(3);
             model.Category = category;
 
-            return View(model); 
+            return View(model);
         }
 
-        public IActionResult Details(int filmId) 
+        public IActionResult Details(int filmId)
         {
             var film = db.Films.Find(filmId);
             var category = db.Kategorie.Find(film.KategoriaId);
 
-            return View(film);        
+            return View(film);
         }
 
-        public IActionResult GetAllFilms() 
+        public IActionResult GetAllFilms()
         {
             var films = db.Films.OrderBy(f => f.Date);
             return View(films);
@@ -53,23 +54,55 @@ namespace PawKarTIWypozyczalnia.Controllers
         public IActionResult AddFilm()
         {
             var model = new AddFilmViewModel();
-            var categories = db.Kategorie.ToList();    
+            var categories = db.Kategorie.ToList();
 
             model.category = categories;
 
-            return View(model); 
+            return View(model);
         }
         [HttpPost]
         public IActionResult AddFilm(AddFilmViewModel model)
         {
             var folderPath = Path.Combine(env.WebRootPath, "content");
-            var posterPath = Path.Combine(folderPath, model.Poster.FileName);
-            model.Poster.CopyTo(new FileStream(posterPath,FileMode.Create));
-            model.newFilm.Date = System.DateTime.Now;
-            model.newFilm.PosterName = model.Poster.FileName;
+            var uniqueName = model.Poster.FileName + "_" + Guid.NewGuid().ToString();
+            var posterPath = Path.Combine(folderPath, uniqueName);
+            model.Poster.CopyTo(new FileStream(posterPath, FileMode.Create));
+            model.newFilm.Date = DateTime.Now;
+            model.newFilm.PosterName = uniqueName;
             db.Films.Add(model.newFilm);
             db.SaveChanges();
-            return RedirectToAction("Indeks", "Home");
+            return RedirectToAction("Index", "Home");
+        }
+        [HttpPost]
+        public IActionResult SearchFilms(string text) 
+        {
+            if (!String.IsNullOrEmpty(text)) 
+            {
+                var film = db.Films.Where(f => f.Title.Contains(text));
+                return View(film.ToList());
+            }
+            return RedirectToAction("Index", "Home");
+        }
+        [HttpGet]
+        public IActionResult EditFilm(int id)
+        {
+            var film = db.Films.Where(_ => _.Id == id).FirstOrDefault();
+
+            return View(film);
+        }
+        [HttpPost]
+        public IActionResult EditFilm(Film film)
+        {
+            var findFilm = db.Films.Where(f => f.Id == film.Id).FirstOrDefault();
+            findFilm.Title = film.Title;
+            findFilm.Director = film.Director;
+            findFilm.Desc = film.Desc;
+            findFilm.Price = film.Price;
+
+            db.Entry(findFilm).State = EntityState.Modified;
+            db.SaveChanges();
+
+            return RedirectToAction("Details", "Films", new { filmId = findFilm.Id });
         }
     }
 }
